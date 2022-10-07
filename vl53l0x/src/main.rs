cbcecf7154ae78d939d73e6eb18d22f05b501205
Smeltie <martijnklerks123@gmail.com>
2022-10-07 19:03:00 +0200

    managed to compile code, currently broken on line 6

--- vl53l0x/src/main.rs
+++ vl53l0x/src/main.rs
@@ -1,18 +1,16 @@
 use vl53l0x::VL53L0x;
 
 fn main() {
-  let _ = VL53L0x::new().unwrap();
-  
-  // let mut tof = VL53L0x::new().unwrap();
+  let mut tof = VL53L0x::new().unwrap();
 
-  // tof.set_measurement_timing_budget(20_000).unwrap();
+  tof.with_address(i2c, 0x29);
+  tof.set_measurement_timing_budget(20_000).unwrap();
+  tof.start_continuous(20).unwrap();
 
-  // tof.start_continuous(20).unwrap();
-
-  // loop {
-  //   match tof.read_range_mm() {
-  //     Ok(range) => println!("{}mm", range),
-  //     Err(e) => println!("{:?}", e),
-  //   }
-  // }
+  loop {
+    match tof.read_range_mm() {
+      Ok(range) => println!("{}mm", range),
+      Err(e) => println!("{:?}", e),
+    }
+  }
 }
