cf8aee8f37be0c5beac40bb304074b3e6527dce0
Smeltie <martijnklerks123@gmail.com>
2022-10-18 20:01:51 +0200

    update time reduced to .25 seconds, currently prints to console when triggered

--- tof/src/main.rs
+++ tof/src/main.rs
@@ -6,26 +6,21 @@ const DEFAULT_ADDRESS: u16 = 0x29;
 
 fn main() -> eyre::Result<()> {
   let mut i2c = I2c::with_bus(1).wrap_err("Error while setting bus")?;
-  let boop_touch = 0..71;
-  let boop_long_range = 76..125;
+  let boop_range = 0..100;
   i2c
     .set_slave_address(DEFAULT_ADDRESS)
     .wrap_err("Error while setting address")?;
   let mut tof = VL53L0x::new(i2c).unwrap();
   println!("chip initialized");
 
-  tof.set_measurement_timing_budget(1_000_000).unwrap();
+  tof.set_measurement_timing_budget(250_000).unwrap();
   tof.start_continuous(0).unwrap();
 
   loop {
     match tof.read_range_continuous_millimeters_blocking() {
       Ok(range) => {
-        if boop_touch.contains(&range) {
-          println!("booped");
-        } else if boop_long_range.contains(&range) {
-          println!("booped hover");
-        } else {
-          println!("not booped");
+        if boop_range.contains(&range) {
+          println!("booped {}", range);
         }
       }
       Err(vl53l0x::Error::Timeout) => continue,
