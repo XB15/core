bf339c2ff84f9443146d09dff3325b6897bd8559
TheDevMinerTV <tobigames200@gmail.com>
2022-11-23 12:05:20 +0100

    impl(testing): simple new_composer demo

--- testing/src/bin/hmm.rs
+++ testing/src/bin/hmm.rs
@@ -1,29 +1,55 @@
-use new_composer::{Composer, Track};
+use std::{
+  sync::{Arc, Mutex},
+  thread,
+  time::Duration,
+};
+
+use new_composer::{Composer, Gif, Track};
+
+fn parse_gif() -> Gif {
+  Gif::new(Vec::new())
+}
 
 fn main() {
-  let eyes = Track::with_idle_animation(Gif::new(Vec::new()))
-    .add_animation(
-      "blink".to_string(),
-      Gif::new(Vec::new()),
-      Gif::new(Vec::new()),
-    )
-    .add_animation("heart", Gif::new(Vec::new()), Gif::new(Vec::new()))
+  let eyes = Track::with_idle_animation(parse_gif())
+    .add_animation("blink".to_string(), parse_gif(), parse_gif())
+    .add_animation("heart", parse_gif(), parse_gif())
     .build();
 
-  let nose = Track::with_idle_animation(Gif::new(Vec::new())).build();
+  let nose = Track::with_idle_animation(parse_gif()).build();
 
-  let mouth = Track::with_idle_animation(Gif::new(Vec::new()))
-    .add_animation("smug", Gif::new(Vec::new()), Gif::new(Vec::new()))
+  let mouth = Track::with_idle_animation(parse_gif())
+    .add_animation("smug", parse_gif(), parse_gif())
     .build();
 
-  let face_features = Track::with_idle_animation(Gif::new(Vec::new()))
-    .add_animation("blush", Gif::new(Vec::new()), Gif::new(Vec::new()))
+  let face_features = Track::with_idle_animation(parse_gif())
+    .add_animation("blush", parse_gif(), parse_gif())
     .build();
 
-  let composer = Composer::builder()
-    .add_track("eyes", eyes)
-    .add_track("nose", nose)
-    .add_track("mouth", mouth)
-    .add_track("face_features", face_features)
-    .build();
+  let composer = Arc::new(Mutex::new(
+    Composer::builder()
+      .add_track("eyes", eyes)
+      .add_track("nose", nose)
+      .add_track("mouth", mouth)
+      .add_track("face_features", face_features)
+      .build(),
+  ));
+
+  let c1 = composer.clone();
+
+  // Rendering thread
+  thread::spawn(move || loop {
+    loop {
+      c1.lock().unwrap().get_pixels_at(Duration::from_secs(0));
+    }
+  });
+
+  composer
+    .lock()
+    .unwrap()
+    .transition_track_to("eyes", "blink");
+
+  loop {
+    thread::sleep(Duration::from_secs(1));
+  }
 }
